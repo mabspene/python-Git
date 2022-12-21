c335d260481f8aac882292288eab326cef05494f
mabspene <mabspene72@gmail.com>
2022-12-21 20:47:45 +0000

    mise jour du nombre aleatoire

--- bouclesWhile.py
+++ bouclesWhile.py
@@ -1,16 +1,18 @@
 import random
 # un programme qui permet de deviner un nombre aléatoire
 
-nb_aleatoire = 0
+nb_aleatoire = int(input("Deviner le nombre aléatoire qui est compris entre 0 et 100 >>\t"))
 aleatoire = random.randrange(100)
 
 while nb_aleatoire != aleatoire:
-    nb_aleatoire = int(input("Deviner le nombre aléatoire qui est compris entre 0 et 100 >>\t"))
+
     if nb_aleatoire < aleatoire:
         print("le nombre saisi est plus petit que le nombre aléatoire.")
+        nb_aleatoire = int(input("Deviner le nombre aléatoire qui est compris entre 0 et 100 >>\t"))
         continue
     elif nb_aleatoire > aleatoire:
         print("le nombre saisi est suppérieur au nombre aléatoire.")
+        nb_aleatoire = int(input("Deviner le nombre aléatoire qui est compris entre 0 et 100 >>\t"))
         continue
     else:
         print("Félicitation vous avez gagner le nombre aléatoire est >>", aleatoire)
